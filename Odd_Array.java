a4e741936445558eb052ba9dfbda0754963e580b
SiddhantOjha17 <96463139+SiddhantOjha17@users.noreply.github.com>
2021-12-24 19:25:25 +0530

    Add files via upload

---
+++ Odd_Array.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.*;
+public class Odd_Array {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of elements you want to store: ");
+//reading the number of elements from the that we want to enter
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for(int i=0; i<n; i++)
+        {
+//reading array elements from the user
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Elements of given array present on odd position: ");
+        //Loop through the array by incrementing value of i by 2
+        for (int i = 0; i < arr.length; i = i+2) {
+            System.out.println(arr[i]);
+        }
+    }
+}
